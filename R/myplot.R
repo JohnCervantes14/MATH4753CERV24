0db5f7bed15853fe010fa10ad948e22e86f10c00
john-cervantes <john.f.cervantes-1@ou.edu>
2024-02-09 14:11:18 -0600

    Lab4 myplot func

---
+++ R/myplot.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param x a numeric vector
+#'
+#' @return a plot of x
+#' @export
+#'
+#' @examples
+#' myplot(x = 1)
+myplot=function(x){
+  0.86089580 +1.46959217*x  -0.02745726*x^2
+}
